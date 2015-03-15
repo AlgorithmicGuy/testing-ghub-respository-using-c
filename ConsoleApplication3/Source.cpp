03cc924e220f80d0cf5d84f8d0fd7121656fdc96
algorithmicguy <cweerathunga@hotmail.com>
2015-03-15 13:28:06 +0530

    algorithmicguy

---
+++ ConsoleApplication3/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int a = 20;
+	int b = 40;
+	auto c = a;
+	int result;
+	b = b + a;
+	result = b - c;
+	string mystring;
+	mystring = "This is just a normal calculation";
+	cout << mystring << endl;
+	cout << result << endl;
+	string mystring2;
+	const double pi = 3.14159;
+	const char newline = '\n';
+	double r = 5.0;
+	double area_r = pow(r, 2);
+	double circle;
+	double area_circle;
+	mystring2 = "Perimeter of a circle with 5cm radius";
+	circle = 2 * pi * r;
+	string mystring3;
+	mystring3 = "Area of the same circle";
+	area_circle = pi * area_r;
+	cout << mystring2;
+	cout << newline;
+	cout << circle << endl;
+	cout << mystring3;
+	cout << newline;
+	cout << area_circle << endl;
+	return 0;
+}
