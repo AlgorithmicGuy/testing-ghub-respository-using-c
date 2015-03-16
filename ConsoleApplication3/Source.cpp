d6f144ff39dd31ad303035e7f7bdde2f6aef9b26
algorithmicguy <cweerathunga@hotmail.com>
2015-03-16 10:19:49 +0530

    Modifications, adding <sstream>

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -1,28 +1,24 @@
 //testing git respository
 #include <iostream>
 #include <string>
+#include <sstream> 
 #include "Header.h"
 using namespace std;
 int main(){
-		int a = 20;
-		int b = 40;
-		auto c = a;
-		int result;
-		b = b + a;
-		result = b - c;
-		string mystring;
-		mystring = "This is just a normal calculation";
-		cout << mystring << endl;
-		cout << result << endl;
+	//A simple code to find perimeter and area of a circle
 		string mystring2;
 		const double pi = 3.14159;
 		const char newline = '\n';
-		double r = 5.0;
-		double area_r = pow(r, 2);
+		string r;
+		int radius = 0;
+		cout << "Radius of the circle? " << endl;
+		getline(cin, r);
+		stringstream(r) >> radius;
+		double area_r = pow(radius, 2);
 		double circle;
 		double area_circle;
 		mystring2 = "Perimeter of a circle with 5cm radius";
-		circle = 2 * pi * r;
+		circle = 2 * pi * radius;
 		string mystring3;
 		mystring3 = "Area of the same circle";
 		area_circle = pi * area_r;
@@ -34,5 +30,22 @@ int main(){
 		cout << area_circle << endl;
 		cout << newline;
 		MyFunc();
+		cout << newline;
+		input();
+		cout << newline;
+		//Another simple mathematical calculation to find the total price
+		string mystring4 = "Price * quantity";
+		cout << mystring4 << endl;
+		cout << newline;
+		string mystring5;
+		float price = 0;
+		int quantity = 0;
+		cout << "Price of the item? "<< endl;
+		getline(cin, mystring5);
+		stringstream(mystring5) >> price;
+		cout << "How many items? " << endl;
+		getline(cin, mystring5);
+		stringstream(mystring5) >> quantity;
+		cout << "Total Price: " << price*quantity << endl;
 		return 0;
 }
