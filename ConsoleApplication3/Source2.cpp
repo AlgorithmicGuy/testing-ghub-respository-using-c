6c891ba7cbbab35a0cf7a1bed81dcc9e4f541852
algorithmicguy <cweerathunga@hotmail.com>
2015-03-16 10:20:42 +0530

    A simple set of codings

--- ConsoleApplication3/Source2.cpp
+++ ConsoleApplication3/Source2.cpp
@@ -2,16 +2,17 @@
 #include <string>
 #define NEWLINE '\n'
 using namespace std;
+//simple program to greet somone
 void input(){
-	cout << "Your name?";
+	cout << "Your name? ";
 	string name;
 	getline(cin, name);
 	cout << NEWLINE;
-	cout << "Hi " << name << ", nice to meet you.";
+	cout << "Hi " << name << ", nice to meet you. ";
 	cout << NEWLINE;
-	cout << name << " what's your favorite subject?";
+	cout << name << " what's your favorite subject? ";
 	string subject;
 	getline(cin, subject);
 	cout << NEWLINE;
-	cout << "I like " << subject << " too.";
+	cout << "I like " << subject << " too. " << endl;
 }
